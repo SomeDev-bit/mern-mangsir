5d14923f571ffe4d84feb345872cc863220e2377
SomeDev-bit <batchdevops431@gmail.com>
2025-02-16 07:12:19 +0545

    all files added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,35 +4,35 @@ import { Button } from '@material-tailwind/react';
 
 const Home = () => {
 
-  // const { isLoading, error, data } = useGetProductsQuery();
-  const [getProducts, { isLoading, data, error }] = useLazyGetProductsQuery();
+  const { isLoading, error, data } = useGetProductsQuery();
+  // const [getProducts, { isLoading, data, error }] = useLazyGetProductsQuery();
 
-  console.log(data);
-
-
+  // console.log(data);
 
 
 
-  // if (isLoading) {
-  //   return <h1>Loading....</h1>
-  // }
 
 
-  // if (error) {
-  //   return <h1>{`${error.data} Status:-${error.status}`}</h1>
-  // }
+  if (isLoading) {
+    return <h1>Loading....</h1>
+  }
+
+
+  if (error) {
+    return <h1>{`${error.data} Status:-${error.status}`}</h1>
+  }
 
 
   return (
     <div className='p-5'>
-      <Button onClick={() => getProducts()}>Submit</Button>
-      {/* 
+      {/* <Button onClick={() => getProducts()}>Submit</Button> */}
+
       {data && data.products.map((product) => {
         return <div key={product.id}>
           <h1>{product.title}</h1>
 
         </div>
-      })} */}
+      })}
 
     </div >
   )
