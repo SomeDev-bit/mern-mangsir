e76089d26dbd993329411c14ef9317c7793c56ec
SomeDev-bit <batchdevops431@gmail.com>
2025-02-18 07:41:22 +0545

    changes

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 import express from 'express';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
-
-
+import 'dotenv/config';
+import mongoose from 'mongoose';
 
 const app = express();
 
+mongoose.connect(process.env.MONGO_URI).then((val) => {
+  app.listen(5000, () => {
+    console.log('Database connected and listening');
+  });
+
+}).catch((err) => {
+  console.log(err);
+});
 
 app.get('/', (req, res) => {
   return res.status(200).json([11, 22, 44, 55]);
@@ -19,6 +27,3 @@ app.use('/api/users', userRoutes);
 
 
 
-app.listen(5000, () => {
-  console.log('listening');
-});
