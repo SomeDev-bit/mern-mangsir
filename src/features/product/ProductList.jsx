7d10835aa9a3fe266b10d8dba48bb3d94ed589a0
SomeDev-bit <batchdevops431@gmail.com>
2025-02-27 07:59:50 +0545

    changes

--- src/features/product/ProductList.jsx
+++ src/features/product/ProductList.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { useGetProductsQuery } from './productApi'
+import { ProductCard } from './ProductCard';
 
 const ProductList = () => {
+
+  const { data, isLoading, error } = useGetProductsQuery();
+
+  if (isLoading) return <h1>Loading...</h1>
+  if (error) return <h1>{error.data?.message}</h1>
   return (
-    <div>
+    <div className='p-5  grid grid-cols-4 gap-4'>
+
+      {data && data.map((product) => {
+        return <ProductCard key={product._id} product={product} />
+      })}
 
     </div>
   )
