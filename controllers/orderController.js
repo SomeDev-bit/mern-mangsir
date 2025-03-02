77ff19bacb7d0e1e4d047f6e14b28177ae32601f
SomeDev-bit <batchdevops431@gmail.com>
2025-03-02 08:02:08 +0545

    changes

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,3 +1,4 @@
+import Order from "../models/Order.js"
 
 
 
@@ -15,5 +16,19 @@ export const getOrderByUser = async (req, res) => {
 }
 
 export const createOrder = async (req, res) => {
+  const { products, totalAmount } = req.body;
 
+  try {
+
+    await Order.create({
+      userId: req.userId,
+      products,
+      totalAmount
+    });
+
+    return res.status(201).json({ message: 'order created successfully' });
+
+  } catch (err) {
+    return res.status(400).json({ message: `${err}` });
+  }
 }
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import { createOrder, getOrderById, getOrderByUser, getOrders } from '../controllers/orderController.js';
+import { authCheck } from '../middlewares/authCheck.js';
 
 
 
 const router = express.Router();
 
 
-router.route('/').get(getOrders).post(createOrder);
-router.route('/users/:id').get(getOrderByUser);
+router.route('/').get(getOrders).post(authCheck, createOrder);
+router.route('/users/:id').get(authCheck, getOrderByUser);
 router.route('/:id').get(getOrderById);
 
 
